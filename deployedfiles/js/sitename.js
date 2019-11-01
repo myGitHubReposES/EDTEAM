3c5c542820fbf2f9b9261740ad03bf9fd4c0af2f
Felix Cordero <altasdeusuario@gmail.com>
2019-11-01 19:13:46 +0100

    Funcion de saludo en la carga de pagina

--- deployedfiles/js/sitename.js
+++ deployedfiles/js/sitename.js
@@ -4,7 +4,9 @@
   *  Version: 1.0
   *  Description: sitename Javascript
   *  ***************************************************************************/
-
+ function saludation(msg) {
+     alert(msg);
+ }
  $(document).ready(function() {
-
+     saludation('Hola mundo');
  });
